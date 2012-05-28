765dbc57c991b2fa40d773416f42b2c070c553ba
Allen.M <menghonglun@gmail.com>
2012-05-28 17:34:34 +0800

    hosts文本编辑框加强，更方便的注释，删除单行记录

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -5,41 +5,134 @@
 
 (function ( $ ) {
 
-    var updateHost = {
-
-        timer:null,
-
+    var hostManage = {
         init:function ( ) {
-            this._initUpdate();
-            /*this._getLastHost();*/
+            this.initInput();
+            this.initItem();
+            this.initSubmit();
         },
-        _initUpdate:function ( ) {
-            var self = this;
-            $('#update-form').on('submit',function ( ev ) {
-                ev.preventDefault();
-                var $form = $(this),
-                    host = $form.find('textarea').val();
-                $.ajax( $form.attr('action'),{
-                    type:"POST",
-                    data: { host:host },
-                    dataType:'json'
 
+        initInput:function ( ) {
+            var self = this;
+            $('#host-input').on('keydown',function ( ev ) {
+                var $this = $(this);
+                
+                // user press enter key , and the cursor in the end
+                if( ev.which === 13 && self._isCursorAtEnd( this ) ){
+                    ev.preventDefault();
+                    self._putTextStored();
+                }
+
+                // user press delete key 
+                if( ev.which === 8 && self._isCursorAtStart( this ) ){
+                    var lastItem = $('#edit-area div.item:last'),
+                        data = lastItem.find('span.data').text();
+
+                    lastItem.remove();
+                    if( $this.val() ){
+                        $this.val( data + '\n' + $this.val() );
+                    }else{
+                        $this.val( data );
+                    }
+                    if( "setSelectionRange" in this ){
+                        this.setSelectionRange( data.length , data.length );
+                    }
+                    ev.preventDefault();
+                }
+
+            });
+        },
+
+        // 把 textarea 中的内容放到 stored 区
+        _putTextStored:function ( ) {
+            var input = $('#host-input'),
+                v = input.val(),
+                items = v.split('\n');
+
+            this._addItems( items );
+            input.val('');
+        },
+
+        _isCursorAtEnd:function ( node ) {
+            // only support the browser which support selectionStart
+            if( "selectionStart" in node ){
+                return ( node.selectionStart === node.value.length && node.selectionStart === node.selectionEnd );
+            }else{
+                return true;
+            }
+        },
+
+        _isCursorAtStart:function ( node ) {
+            // only support the browser which support selectionStart
+            if( "selectionStart" in node ){
+                return (node.selectionStart === 0 && node.selectionStart === node.selectionEnd );
+            }else{
+                return false;
+            }
+        },
+        
+        
+        _addItems:function ( items ) {
+            var html = [];
+            $.each( items , function ( i, item ) {
+                if( item ){
+                    if( item.charAt(0) !== '#' ){
+                        html.push('<div class="item clearfix"><span class="data">'+item+'</span><a href="#" class="delete">\u00d7</a></div>');
+                    }else{
+                        html.push('<div class="unactive-item item clearfix"><span class="data">'+item+'</span><a href="#" class="delete">\u00d7</a></div>');
+                    }
+                }
+            });
+            $('#edit-area div.stored').append( html.join('') );
+        },
+
+        initItem:function ( ) {
+            $('#edit-area').on('click','div.item',function ( ev ) {
+                var target = ev.target,
+                    $this = $(this);
+
+                ev.preventDefault();
+                if( $(target).hasClass('delete') ){
+                    $this.remove();
+                }else if( $this.hasClass('unactive-item') ){
+                    $this.removeClass('unactive-item');
+                    $this.find('span.data').text( $this.find('span.data').text().replace(/^#+/,"") );
+                }else{
+                    $this.addClass('unactive-item');
+                    $this.find('span.data').text( '#'+$this.find('span.data').text() );
+                }
+            });
+        },
+
+        initSubmit:function ( ) {
+            var self = this;
+            $('#submit-btn').on('click',function ( ev ) {
+                ev.preventDefault();
+                self._putTextStored();
+                var hosts = [],
+                    url = "/update-host";
+                $('#edit-area span.data').each(function ( index, item ) {
+                    hosts.push( $(this).text() );
+                });
+
+                $.ajax( url, {
+                    type:"POST",
+                    data: {
+                        host: hosts.join('\n')
+                    },
+                    dataType:"json"
                 }).done(function ( data ) {
                     if( data.success === true ){
-                        window.localStorage.setItem('host',host);
                         self.toast("同步成功");
                     }else{
                         self.toast("同步失败","warning");
                     }
                 });
+
+
             });
         },
 
-        _getLastHost:function ( ) {
-            var host = window.localStorage.getItem('host');
-            $('#update-form textarea').val( host );
-        },
-        
         toast:function ( txt, className ) {
             var className = className || "normal";
             if( this.timer ){
@@ -52,8 +145,9 @@
         }
     };
 
-    $( function ( ) {
-        updateHost.init();
-    });
 
+    $( function ( ) {
+        hostManage.init();
+    });
+    
 })( jQuery );
